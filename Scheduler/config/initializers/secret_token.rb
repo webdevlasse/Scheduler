b10683ca142cb94fc046acdc6293b18fe34bd649
Apprentice <apprentice@dbc04.local>
2013-01-18 14:40:43 -0800

    initial commit

---
+++ Scheduler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = '1dbb7a5f166ea90b67914bc767089803a9064ced8ab8067b894e5f9c84427c85be1c58a57cff30a7338dfb0308948d1e9928f21658721d85ac0b2be240f2b51c'
