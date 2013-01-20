79bbc29b075bf5037a24edcba677956d521bc653
Apprentice <apprentice@devbootcamp.com>
2013-01-19 16:31:06 -0800

    modal added

--- Scheduler/app/controllers/appointments_controller.rb
+++ Scheduler/app/controllers/appointments_controller.rb
@@ -1,2 +1,22 @@
 class AppointmentsController < ApplicationController
+
+  def index
+    @appointments = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def create
+    @appointment = Appointment.new(params[:appointment])
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appintment.destroy
+      respond_to do |format|
+        format.js
+      end
+  end
 end
--- Scheduler/app/models/appointment.rb
+++ Scheduler/app/models/appointment.rb
@@ -1,3 +1,10 @@
 class Appointment < ActiveRecord::Base
   attr_accessible :end_time, :instructor, :start_time
+
+
+  def duration
+    @end_time - @start_time
+  end
+
+
 end
